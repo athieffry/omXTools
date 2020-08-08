4acde338bb56154f0410094165c150a5580be219
Axel Thieffry <axel.thieffry@gmail.com>
2020-08-08 11:19:00 +0200

    Update samtobam.sh
    CPU detection for Mac/Linux added

--- samtobam.sh
+++ samtobam.sh
@@ -2,33 +2,49 @@
 # Axel Thieffry - August 2020
 set -e
 set -u
-VERSION="1.0"
-CPU_SYSTEM=$(nproc)
+VERSION="1.1"
 VERBOSE=0
 KEEP=0
 DEBUG=0
 
+# detect if Mac or Linux OS
+if [[ "$(uname -s)" == "Darwin" ]]
+then
+	CPU_SYSTEM=$(sysctl -n hw.logicalcpu)
+else
+	CPU_SYSTEM=$(nproc)
+fi
+
+
 # USAGE
 usage() {
-	echo "samtobam.sh converts SAM into sorted and indexed BAM."
-	echo "  Author: Axel Thieffry"
-	echo "  version: $VERSION"
+	echo "NAME"
+	echo "    samtobam.sh - converts SAM into sorted and indexed BAM."
+	echo "SYNOPSIS"
+	echo "    samtobam.sh [-hvk] [-p <int>] -i <input.sam>"
+	echo "DESCRIPTION"
+	echo "    Takes a SAM alignment file and converts it into BAM format."
+	echo "    The resulting BAM is then sorted and indexed."
+	echo "OPTIONS"
+	echo "    -i  SAM input file."
+	echo "    -p  Specify the number of CPUs to be used (integer)."
+	echo "        Default: detected from system."
+	echo "    -v  Enable verbose output to stdout."
+	echo "        Default: silent."
+	echo "    -k  Keep original SAM file."
+	echo "        Default: removes SAM file."
+	echo "    -h  Show this help."
 	echo ""
-	echo "USAGE: $ samtobam.sh [-hvk] [-p <int>] -i <input.sam>"
-	echo "  -i     SAM input file."
-	echo "  -p     Specify the number of CPUs to be used (integer)."
-	echo "         Default: detected by calling nproc."
-	echo "  -v     Enable verbose output to stdout."
-	echo "         Default: silent."
-	echo "  -k     Keep original SAM file."
-	echo "         Default: removes SAM file."
-	echo "  -h     Show this help."
+	echo "Note: this script will create (temporary) intermediate files."
+	echo "Be sure to have enough disk space."
 	echo ""
-	echo "Warnings: this script will create (temporary) intermediate files. Be sure to have enough disk space."
+	echo "Author: Axel Thieffry"
+	echo "version: $VERSION"
  	}
 
 # check if any argument has been provided
-if [[ $# -eq 0 ]] ; then
+if [[ $# -eq 0 ]]
+then
 	usage
 	echo "samtobam.sh requires an input file" >&2
 	exit 1
@@ -42,13 +58,15 @@ while getopts ":i:p:kvh" opts
 		  INPUT=$OPTARG
 
 		  # check that SAM input file exists
-		  if ! [[ -f "$INPUT" ]]; then
+		  if ! [[ -f "$INPUT" ]]
+		  then
 		  	echo "SAM input file not found ($INPUT)." >&2
 			exit 1
 		  fi
 
 		  # check that input_file.bam does not already exist
-		  if [[ -f ${INPUT%.*}.bam ]]; then
+		  if [[ -f ${INPUT%.*}.bam ]]
+		  then
 			echo "BAM file seems to already exist for this input ($INPUT)." >&2
 			exit 1
 		  fi
@@ -58,7 +76,8 @@ while getopts ":i:p:kvh" opts
 		  CPU_USER=${OPTARG%.*}
 
 		  # check that number of CPU is greater than 1
-		  if [[ $CPU_USER -lt "1" ]]; then
+		  if [[ $CPU_USER -lt "1" ]]
+		  then
 			echo "Provided CPU number ($CPU_USER) must be greater than 0." >&2
 			echo ""
 			usage
@@ -66,7 +85,8 @@ while getopts ":i:p:kvh" opts
 		  fi
 
 		  # check that number of CPU is not greater than what the system has
-		  if [[ $CPU_USER -gt $CPU_SYSTEM ]]; then
+		  if [[ $CPU_USER -gt $CPU_SYSTEM ]]
+		  then
 			echo "Provided CPU number ($CPU_USER) cannot exceed available CPUs ($CPU_SYSTEM)." >&2
 			usage
 			exit 1
@@ -91,11 +111,17 @@ while getopts ":i:p:kvh" opts
 	done
 
 # if user provided senseful number of CPUs, use it, otherwise fall back on system
-if [[ -z ${CPU_USER+x} ]]; then CPU=$CPU_SYSTEM; else CPU=$CPU_USER ; fi
+if [[ -z ${CPU_USER+x} ]]
+then
+	CPU=$CPU_SYSTEM
+else
+	CPU=$CPU_USER
+fi
 
 
 # low-tech DEBUG
-if [[ $DEBUG -eq 1 ]] ; then
+if [[ $DEBUG -eq 1 ]]
+then
 	echo ""
 	echo "//DEBUG//"
 	echo "INPUT: ${INPUT:-}"
@@ -107,7 +133,8 @@ if [[ $DEBUG -eq 1 ]] ; then
 fi
 
 # check if samtools can be found in PATH
-if ! [[ -x "$(command -v samtools)" ]]; then
+if ! [[ -x "$(command -v samtools)" ]]
+then
 	echo "SAMTOOLS command could not be found." >&2
 	echo "Make sure samtools is installed and accessible within PATH." >&2
 	exit 1
@@ -122,24 +149,44 @@ BAM=${INPUT%.*}.bam
 SORTEDBAM=${INPUT%.*}.sorted.bam
 
 # 1. sam to bam
-if [[ $VERBOSE -eq 1 ]]; then echo "Converting SAM into BAM..."; fi
+if [[ $VERBOSE -eq 1 ]]
+	then echo "Converting SAM into BAM..."
+fi
+
 samtools view -b $SAM -o $BAM -@ $CPU
 
 # 2. remove input.sam
-if [[ $VERBOSE -eq 1 ]]; then echo "Removing input SAM..."; fi
-if [[ $KEEP -eq 0 ]]; then rm $INPUT; fi
+if [[ $VERBOSE -eq 1 ]]
+then
+	echo "Removing input SAM..."
+fi
+
+if [[ $KEEP -eq 0 ]]
+then
+	rm $INPUT
+fi
 
 # 3. sort and rename
-if [[ $VERBOSE -eq 1 ]]; then echo "Sorting and renaming BAM..."; fi
+if [[ $VERBOSE -eq 1 ]]
+then
+	echo "Sorting and renaming BAM..."
+fi
+
 samtools sort $BAM -l 9 -o $SORTEDBAM -@ $CPU
 mv $SORTEDBAM $BAM
 
 # 4. index
-if [[ $VERBOSE -eq 1 ]]; then echo "Indexing BAM..."; fi
+if [[ $VERBOSE -eq 1 ]]
+then
+	echo "Indexing BAM..."
+fi
+
 samtools index $BAM -@ $CPU
-if [[ $VERBOSE -eq 1 ]]; then echo "~DONE~"; fi
 
-
+if [[ $VERBOSE -eq 1 ]]
+then
+	echo "~DONE~"
+fi
 
 exit 0
 ### EOF ###
